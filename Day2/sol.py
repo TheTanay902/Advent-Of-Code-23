1aff01fe66e46aac42e850f2a4ae3f17ad4fbfe1
Tanay Toshniwal <tanaytoshniwal902@gmail.com>
2023-12-06 12:17:21 +0530

    Modify operations to solve D2P2

--- Day2/sol.py
+++ Day2/sol.py
@@ -1,11 +1,10 @@
-n_red = 12
-n_green = 13
-n_blue = 14
 file_input = open("advent.txt", "r")
 input_list = file_input.readlines()
 valid_id_sum = 0
 for game in input_list:
-    custom_flag = True
+    max_red = 0
+    max_blue = 0
+    max_green = 0
     gamessplit = game.split(": ")
     game_id = int(gamessplit[0].split(" ")[1])
     ListOfRounds = gamessplit[1].split("; ")
@@ -14,14 +13,11 @@ for game in input_list:
         for val in round_split:
             count, color = val.split(" ")
             if (color[0]=='r'):
-                if(int(count)>n_red):
-                    custom_flag=False
+                max_red = max(max_red, int(count))
             if (color[0]=='g'):
-                if(int(count)>n_green):
-                    custom_flag=False
+                max_green = max(max_green, int(count))
             if (color[0]=='b'):
-                if(int(count)>n_blue):
-                    custom_flag=False
-    if(custom_flag):
-        valid_id_sum+=game_id
+                max_blue = max(max_blue, int(count))
+    power = max_blue*max_green*max_red
+    valid_id_sum+=power
 print(valid_id_sum)
